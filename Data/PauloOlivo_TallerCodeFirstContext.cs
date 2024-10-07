48fe64d06e09b4f5871eff708f996495e61af0a3
laboratorio <laboratorio@UPEAULA-31715>
2024-10-07 11:17:52 -0500

    Agregar archivos de proyecto.

---
+++ Data/PauloOlivo_TallerCodeFirstContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PauloOlivo_TallerCodeFirst.Models;
+
+namespace PauloOlivo_TallerCodeFirst.Data
+{
+    public class PauloOlivo_TallerCodeFirstContext : DbContext
+    {
+        public PauloOlivo_TallerCodeFirstContext (DbContextOptions<PauloOlivo_TallerCodeFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PauloOlivo_TallerCodeFirst.Models.Burger> Burger { get; set; } = default!;
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PauloOlivo_TallerCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
